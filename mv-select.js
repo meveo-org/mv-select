ceef9a113773bc3f4e58f445ff673c9ffd9cf3cc
Paul Stobart <paul.stobart@webdrone.fr>
2022-01-03 16:30:39 +0100

    Update npm dependencies

--- mv-select.js
+++ mv-select.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from "lit-element";
+import { LitElement, html, css } from "lit";
 import { debounce } from "./lib/debounce.js";
 import "@meveo-org/mv-click-away";
 
