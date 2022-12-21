81fdcf88795b186f11bb51092c37e8b0190cc5c3
Guillaume Guarin <guillaume.guarin@webdrone.fr>
2022-12-21 09:13:18 +0100

    dropdown position relative

--- mv-select.js
+++ mv-select.js
@@ -226,8 +226,7 @@ export class MvSelect extends LitElement {
       .mv-select-options.open {
         box-shadow: 3px 3px 10px 0px rgba(58, 58, 58, 0.6);
         z-index: 10;
-        position: absolute;
-        top:35px;
+        position: relative;
       }
 
       .mv-select-item {
